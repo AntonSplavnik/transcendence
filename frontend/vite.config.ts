99eef0740737dbf1e401566f99c8d2fd6f3489a7
Moat <schluesselfehlt@gmail.com>
2025-12-17 17:01:45 +0100

    feat(dynamic frontend) npm run dev now can be used in conjunction with backend
    also made a npm run all that should start front and backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	server: {
+		proxy: {
+			'/api': {
+				target: 'https://127.0.0.1:8443',
+				changeOrigin: true,
+				secure: false,
+			},
+		},
+	},
 })
