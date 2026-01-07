02838b8d731868019181226efc33a061278d1771
Moat <schluesselfehlt@gmail.com>
2026-01-07 17:03:28 +0100

    feat(nickname checking) added valid nickname indicator to authpage
    using api/users/nickname-exists to check if nickname is misconfigured or
    in use and displaying to client "checking" or correct or misconfigured
    or taken

---
+++ frontend/src/api/users.ts
@@ -0,0 +1,28 @@
+import apiClient from './client';
+
+export async function nicknameExists(nickname: string): Promise<string> {
+	try {
+		const response = await apiClient.post<{ exists: boolean, valid: boolean }>('users/nickname-exists',
+			nickname,
+			{
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			}
+		);
+		const data = response.data;
+		if (typeof data.exists !== "boolean" || typeof data.valid !== "boolean") {
+			return "Unexpected server response";
+		}
+		if (data.exists) {
+			return "❌ nickname already taken";
+		}
+		else if (!data.valid) {
+			return "❌ nickname format invalid";
+		}
+		return "✅";
+	} catch (error) {
+		console.error("Nickname validation error:", error);
+		return "Error checking nickname";
+	}
+}
