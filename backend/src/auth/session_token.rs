5bfb353d1649b66f5f5075284bf177a3ca6b07ad
Kajus Wurster <kjzl@posteo.de>
2026-02-08 14:18:55 +0100

    refactor(be): refactor SessionTokenHash and SqlUlid and RandomNonce to use FixedBlob<N>

--- backend/src/auth/session_token.rs
+++ backend/src/auth/session_token.rs
@@ -1,13 +1,8 @@
 use base64::engine::general_purpose::URL_SAFE_NO_PAD as base64url;
 use base64::prelude::*;
-use diesel::backend::Backend;
-use diesel::deserialize::{FromSql, FromSqlRow};
-use diesel::expression::AsExpression;
-use diesel::serialize::ToSql;
-use diesel::sql_types::Binary;
-use diesel::sqlite::Sqlite;
 use thiserror::Error;
 
+use crate::models::blob::FixedBlob;
 use crate::prelude::*;
 
 #[derive(Debug, Error)]
@@ -65,46 +60,20 @@ impl TryFrom<String> for SessionToken {
     type Error = TokenDecodeError;
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, FromSqlRow, AsExpression)]
-#[diesel(sql_type = Binary)]
-pub struct SessionTokenHash([u8; 32]);
-
-impl<DB> ToSql<Binary, DB> for SessionTokenHash
-where
-    DB: diesel::backend::Backend,
-    [u8; 32]: ToSql<Binary, DB>,
-{
-    fn to_sql<'b>(
-        &'b self,
-        out: &mut diesel::serialize::Output<'b, '_, DB>,
-    ) -> diesel::serialize::Result {
-        self.0.to_sql(out)
-    }
-}
-
-impl FromSql<Binary, Sqlite> for SessionTokenHash
-where
-    Vec<u8>: FromSql<Binary, Sqlite>,
-{
-    fn from_sql(
-        mut bytes: <Sqlite as Backend>::RawValue<'_>,
-    ) -> diesel::deserialize::Result<Self> {
-        Ok(SessionTokenHash(
-            bytes
-                .read_blob()
-                .try_into()
-                .map_err(|_| "SessionTokenHash blob length incorrect")?,
-        ))
-    }
-}
+#[derive(
+    diesel_derive_newtype::DieselNewType,
+    Debug,
+    Clone,
+    Copy,
+    PartialEq,
+    Eq,
+    Hash,
+)]
+pub struct SessionTokenHash(FixedBlob<32>);
 
 impl SessionTokenHash {
     pub fn to_truncated(&self) -> SessionTokenHashTruncated {
         SessionTokenHashTruncated::from(*self)
-    }
-
-    pub fn encoded(&self) -> String {
-        base64url.encode(&self.0)
     }
 }
 
@@ -112,21 +81,6 @@ impl From<SessionToken> for SessionTokenHash {
     fn from(value: SessionToken) -> Self {
         Self(blake3::hash(&value.0).into())
     }
-}
-
-impl TryFrom<String> for SessionTokenHash {
-    fn try_from(s: String) -> Result<Self, Self::Error> {
-        let decoded = base64url.decode(s.as_bytes())?;
-        if decoded.len() != 32 {
-            return Err(TokenDecodeError::InvalidLength {
-                expected: 32,
-                actual: decoded.len(),
-            });
-        }
-        Ok(Self(decoded.try_into().expect("length checked")))
-    }
-
-    type Error = TokenDecodeError;
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize, Serialize)]
@@ -180,5 +134,11 @@ impl PartialEq<SessionTokenHashTruncated> for SessionTokenHash {
 impl PartialEq<SessionTokenHash> for SessionTokenHashTruncated {
     fn eq(&self, other: &SessionTokenHash) -> bool {
         &self.0[..] == &other.0[..16]
+    }
+}
+
+impl From<blake3::Hash> for FixedBlob<32> {
+    fn from(value: blake3::Hash) -> Self {
+        FixedBlob(value.into())
     }
 }
--- backend/src/models/ulid.rs
+++ backend/src/models/ulid.rs
@@ -1,12 +1,11 @@
-use diesel::{
-    backend::Backend, deserialize::FromSql, serialize::ToSql,
-    sql_types::Binary, sqlite::Sqlite,
-};
+use diesel_derive_newtype::DieselNewType;
 use serde::{Deserialize, Serialize};
 use ulid::Ulid;
 
+use crate::models::blob::FixedBlob;
+
 #[derive(
-    Debug,
+    DieselNewType,
     Clone,
     Copy,
     PartialEq,
@@ -14,59 +13,44 @@ use ulid::Ulid;
     PartialOrd,
     Ord,
     Hash,
-    diesel::expression::AsExpression,
-    diesel::deserialize::FromSqlRow,
     Serialize,
     Deserialize,
 )]
-#[diesel(sql_type = diesel::sql_types::Binary)]
 #[serde(from = "Ulid")]
 #[serde(into = "Ulid")]
-pub struct SqlUlid([u8; 16]);
+pub struct SqlUlid(FixedBlob<16>);
 
 impl SqlUlid {
+    #[inline]
     pub fn ulid(&self) -> Ulid {
-        Ulid::from_bytes(self.0)
+        Ulid::from_bytes(self.0.0)
     }
 }
 
 impl From<Ulid> for SqlUlid {
+    #[inline]
     fn from(value: Ulid) -> Self {
-        Self(value.to_bytes())
+        Self(FixedBlob(value.to_bytes()))
     }
 }
 
 impl From<SqlUlid> for Ulid {
+    #[inline]
     fn from(value: SqlUlid) -> Self {
-        value.0.into()
+        value.0.0.into()
     }
 }
 
-impl<DB> ToSql<Binary, DB> for SqlUlid
-where
-    DB: Backend,
-    [u8; 16]: ToSql<Binary, DB>,
-{
-    fn to_sql<'b>(
-        &'b self,
-        out: &mut diesel::serialize::Output<'b, '_, DB>,
-    ) -> diesel::serialize::Result {
-        self.0.to_sql(out)
+impl std::fmt::Debug for SqlUlid {
+    #[inline]
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.ulid().fmt(f)
     }
 }
 
-impl FromSql<Binary, Sqlite> for SqlUlid
-where
-    Vec<u8>: FromSql<Binary, Sqlite>,
-{
-    fn from_sql(
-        mut bytes: <Sqlite as Backend>::RawValue<'_>,
-    ) -> diesel::deserialize::Result<Self> {
-        Ok(Self(
-            bytes
-                .read_blob()
-                .try_into()
-                .map_err(|_| "Ulid blob length incorrect")?,
-        ))
+impl std::fmt::Display for SqlUlid {
+    #[inline]
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.ulid().fmt(f)
     }
 }
