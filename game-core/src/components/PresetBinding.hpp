4e0db7e0988be19aefe57cad988c63ea0028a536
LazyPixel <antonsplavnik@gmail.com>
2026-04-23 20:33:25 +0200

    docs: remove stale hot-reload TODO from PresetBinding

--- game-core/src/components/PresetBinding.hpp
+++ game-core/src/components/PresetBinding.hpp
@@ -6,8 +6,6 @@ namespace ArenaGame {
 namespace Components {
 
 // Preset id (filename stem of the JSON file the entity was spawned from).
-// Today this is informational. Future hot-reload will query entities by id
-// to refresh preset-sourced fields after a file change.
 struct PresetBinding {
 	std::string id;
 };
